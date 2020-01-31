8b0f5c259ad1240c3fa3dce36bcf290cb3a195d7
Nicolas Timm <nntimm1@gmail.com>
2020-01-31 15:40:54 -0600

    Added Contestant Class.

---
+++ Class1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SweepstakesApp
+{
+    class Class1
+    {
+        // -------------------------------- Member Variable -------------------------------
+
+
+
+        // -------------------------------- Constructor -----------------------------------
+
+
+
+
+        // -------------------------------- Member Method ---------------------------------
+    }
+}
---
+++ Contestants.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SweepstakesApp
+{
+    class Contestants
+    {
+        // -------------------------------- Member Variable -------------------------------
+        public string firstName;
+        public string lastName;
+        public string emailAddress;
+        public int registrationNumber;
+
+
+        // -------------------------------- Constructor -----------------------------------
+
+        public Contestants()
+        {
+            registrationNumber = 00000;
+        }
+
+
+        // -------------------------------- Member Method ---------------------------------
+    }
+}
